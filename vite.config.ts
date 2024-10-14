268468f6790e0ca607a9b4527bf984533113f7c8
Abhishek kumar mehto <abhi1260404@gmail.com>
2024-10-14 13:00:53 +0530

    update package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000, // Use Render's provided port or fall back to 3000
+    host: '0.0.0.0', // Bind to 0.0.0.0
+  }
 })
